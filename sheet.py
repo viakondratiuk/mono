87eac74fac31d85210822566b92704c1d4f7e0bc
Viacheslav Kondratiuk <viakondratiuk@gmail.com>
2024-01-24 19:46:44 +0200

    Read at mono, write to sheets

--- sheet.py
+++ sheet.py
@@ -9,11 +9,11 @@ def save(body: str) -> None:
     service = build('sheets', 'v4', credentials=credentials)
 
     # Specify the range and values to be updated
-    range_ = 'Sheet1!A1:D5'  # Example range
+    range_ = 'Sheet1!A2:M5'  # Example range
 
     # Call the Sheets API to update the range
     result = service.spreadsheets().values().update(
         spreadsheetId=SHEETS_ID, range=range_,
         valueInputOption='RAW', body=body).execute()
 
-    print(f"{result.get('updatedCells')} cells updated.")
+    print(f"Sheets: updated {result.get('updatedCells')} cells.")
--- sync.py
+++ sync.py
@@ -1,19 +1,42 @@
-from pprint import pprint
+import json
 
 import mono
 import sheet
 from settings import get_mono_key
 
 
-# mono_key = get_mono_key()
-# transactions = mono.get_transactions(mono_key)
+mono_key = get_mono_key()
+transactions = mono.get_transactions(mono_key)
+print(f"Mono: Got {len(transactions)} transactions")
 
-# pprint(transactions)
+# TODO: Save as debug part
+# with open('transactions.json', 'w') as f:
+#     json.dump(transactions, f, indent=4)
+
+# with open('transactions.json', 'r') as f:
+#     transactions = json.load(f)
+
+values = []
+for transaction in transactions:
+    row = [
+        transaction["id"],
+        transaction["time"],
+        transaction["description"],
+        transaction["mcc"],
+        transaction["originalMcc"],
+        transaction["amount"],
+        transaction["operationAmount"],
+        transaction["currencyCode"],
+        transaction["commissionRate"],
+        transaction["cashbackAmount"],
+        transaction["balance"],
+        transaction["hold"],
+        transaction["receiptId"],
+    ]
+    values.append(row)
 
 body = {
-    'values': [
-        ['A1', 'B1', 'C1', 'D1'],
-        ['A2', 'B2', 'C2', 'D2'],
-    ]
+    'values': values
 }
+
 sheet.save(body)
