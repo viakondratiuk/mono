92f2f512c4f0e69c353bc2f6c20a58089c030a59
Viacheslav Kondratiuk <viacheslav.kondratiuk@bizi.co.il>
2024-01-30 14:26:21 +0200

    Refactor

--- settings.py
+++ settings.py
@@ -3,11 +3,11 @@ import logging
 
 from google.oauth2.service_account import Credentials
 
-MONO_KEY_PATH = "keys/mono.json"
+MONO_CREDENTIALS = "keys/mono.json"
 MONO_ACCOUNT = "0"
 MONO_API_URL = "https://api.monobank.ua/personal/statement/{}/{}/{}"
 
-SHEETS_SERVICE_ACCOUNT = "keys/sheets.json"
+SHEETS_CREDENTIALS = "keys/sheets.json"
 SHEETS_SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SHEETS_ID = "1L1f-c31oWSG-CTGuSe1XqSAWWHwX6_7XiUTlBoeHh3E"
 
@@ -20,11 +20,10 @@ logger = logging.getLogger(__name__)
 
 
 def get_mono_key() -> str:
-    with open(MONO_KEY_PATH, "r") as file:
+    with open(MONO_CREDENTIALS, "r") as file:
         data = json.load(file)
-
-    return data["API_KEY"]
+        return data.get("API_KEY")
 
 
 def get_sheet_credentials():
-    return Credentials.from_service_account_file(SHEETS_SERVICE_ACCOUNT, scopes=SHEETS_SCOPES)
+    return Credentials.from_service_account_file(SHEETS_CREDENTIALS, scopes=SHEETS_SCOPES)
